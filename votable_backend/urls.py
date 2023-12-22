12aa10ca34a5327c4fb2b0c9e0672f08a59c4add
rob-hayward <hayward.m.rob@gmail.com>
2023-12-22 23:20:43 +0000

    added "path('auth/', include('djoser.urls'))" to project urls. Backend seems to be mainly rebuilt and operational. Navbar needs fixing and .env for database and secret key needs implementing.

--- votable_backend/urls.py
+++ votable_backend/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/', include('djoser.urls')),
     path('api/v1/', include('votable_app.urls')),
     path('auth/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
